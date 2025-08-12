b70ba15bacf17ac30541150b286b37305a80f8da
Ashis Kumar Ray <ashisray.057@gmail.com>
2025-08-12 14:28:42 +0530

    initial commit

---
+++ multiple_attach.py
@@ -0,0 +1,35 @@
+import smtplib
+import ssl
+import mimetypes
+from email.message import EmailMessage
+
+email=EmailMessage()
+
+subject = "just look at this"
+email.set_content(subject)
+
+files = [r"C:\Users\ashis\OneDrive\Pictures\wp7102558-jai-shree-ram-4k-wallpapers.png",r"C:\Users\ashis\OneDrive\Pictures\Camera Roll\WIN_20250103_18_21_03_Pro.jpg"]
+for file_path in files :
+    with open (file_path,"rb") as f:
+        file_data=f.read()
+        file_name=f.name
+        mime_type,_=mimetypes.guess_type(file_path)
+        maintype,subtype=mime_type.split('/')
+    email.add_attachment(file_data,maintype=maintype,subtype=subtype,filename=file_name)
+
+sender_email="dataautomation.py@gmail.com"
+receiver_email="raydit.business@gmail.com"
+app_password="bysrdzjryxrlaluo"
+
+email['from']=sender_email
+email['to']=receiver_email
+email['subject']=subject
+
+context= ssl.create_default_context() 
+with smtplib.SMTP_SSL("smtp.gmail.com",465,context =context) as smtp :
+    smtp.login(sender_email,app_password)
+    smtp.send_message(email)
+
+print ("done brooooo")
+
+    
